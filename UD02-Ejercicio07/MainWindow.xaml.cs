3a553fed7823db9867051f2dda14ef10118c1733
Francisco Julian Gil Marin <fj.gil16@iesdoctorbalmis.com>
2020-10-24 14:15:20 +0200

    Agregar archivos de proyecto.

---
+++ UD02-Ejercicio07/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace UD02_Ejercicio07
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TamanoRadioBoton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rd = (sender as RadioButton);
+            if ((bool)rd.IsChecked)
+                textoTextBlock.FontSize = int.Parse(rd.Tag.ToString());
+        }
+    }
+}
